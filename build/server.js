dd5fa9a6e8cd3cf2672f564e8a8573c219cd6501
Tanesal <dguchinsky@yahoo.com>
2021-06-13 16:16:09 +0600

    refactor: delete build

--- build/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("./common/config");
-const app_1 = require("./app");
-app_1.app.listen(config_1.PORT || 4000, () => console.log(`App is running on http://localhost:`));
-//# sourceMappingURL=server.js.map
