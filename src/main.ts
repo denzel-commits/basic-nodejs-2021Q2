f5856b95159618ecfe249af6f3ff1c399f680440
Tanesal <dguchinsky@yahoo.com>
2021-07-11 02:49:56 +0600

    refactor: change httpadapter use

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule } from '@nestjs/swagger';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { ExpressAdapter } from '@nestjs/platform-express/adapters';
 import { AppModule } from './app.module';
 import * as YAML from 'yamljs';
 import * as path from 'path';
@@ -14,38 +12,19 @@ import { configuration } from './config/configuration';
 const DIR_NAME = path.resolve(path.dirname(''));
 const swaggerDocument = YAML.load(path.join(DIR_NAME, './doc/api.yaml'));
 
-async function expressBootstrap() {
-  console.log('Use Express');
-  let app: Promise<any>;
-  app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  app.useGlobalPipes(new ValidationPipe());
+async function bootstrap() {
+  const httpAdapter = configuration.useFastify()
+    ? FastifyAdapter
+    : ExpressAdapter;
 
-  const document = SwaggerModule.createDocument(app, swaggerDocument);
-  SwaggerModule.setup('doc', app, document);
-  await app.listen(configService.get<number>('PORT'));
-}
-
-async function fastifyBootstrap() {
-  console.log('Use Fastify');
-
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule, new httpAdapter());
 
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
 
   const document = SwaggerModule.createDocument(app, swaggerDocument);
   SwaggerModule.setup('doc', app, document);
+
   await app.listen(configService.get<number>('PORT'), '0.0.0.0');
 }
-
-const useFastify = configuration.useFastify();
-
-if (useFastify) {
-  fastifyBootstrap();
-} else {
-  expressBootstrap();
-}
+bootstrap();
